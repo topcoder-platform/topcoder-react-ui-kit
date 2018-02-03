fbc3a844b34aa5c2e3f7fb1b6fdb457d8eb57a29
Dr. Sergey Pogodin <dr.pogodin@gmail.com>
2018-02-03 22:45:35 +0100

    A few small fixes

--- src/shared/components/Avatar/index.jsx
+++ src/shared/components/Avatar/index.jsx
@@ -1,9 +1,12 @@
 import DefaultUserAvatar from 'assets/images/default-user-avatar.svg';
 import React from 'react';
 import { themr } from 'react-css-super-themr';
-import { Avatar } from 'topcoder-react-utils';
+import { Avatar as ProtoAvatar } from 'topcoder-react-utils';
 
 import theme from './style.scss';
 
-export default themr('Avatar', theme)(props =>
-  <Avatar DefaultAvatar={DefaultUserAvatar} {...props} />);
+function Avatar(props) {
+  return <ProtoAvatar DefaultAvatar={DefaultUserAvatar} {...props} />;
+}
+
+export default themr('Avatar', theme)(Avatar);
