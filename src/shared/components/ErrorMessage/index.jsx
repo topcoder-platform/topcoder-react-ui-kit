8ed26042b0b83a1e0befc7267adc2a816b17cd9c
lunarkid <4476442+dedywahyudi@users.noreply.github.com>
2022-09-23 09:05:10 +0700

    fix: private challenge message

--- src/shared/components/ErrorMessage/index.jsx
+++ src/shared/components/ErrorMessage/index.jsx
@@ -26,6 +26,7 @@ class ErrorMessage extends React.Component {
     const {
       title,
       details,
+      support,
       onOk,
     } = this.props;
 
@@ -33,12 +34,20 @@ class ErrorMessage extends React.Component {
       <Modal theme={{ container: style.container }}>
         <p styleName="title">{title}</p>
         <p styleName="details">{details}</p>
-        <p styleName="details">
-          We are sorry that you have encountered this problem. Please, contact
-          our support &zwnj;
-          <a href="mailto:support@topcoder.com">support@topcoder.com</a>
-          &zwnj; to help us resolve it as soon as possible.
-        </p>
+        {
+          support ? (
+            <p styleName="details">
+              {support}
+            </p>
+          ) : (
+            <p styleName="details">
+              We are sorry that you have encountered this problem. Please, contact
+              our support &zwnj;
+              <a href="mailto:support@topcoder.com">support@topcoder.com</a>
+              &zwnj; to help us resolve it as soon as possible.
+            </p>
+          )
+        }
         <DangerButton
           onClick={(e) => {
             e.preventDefault();
@@ -59,6 +68,7 @@ ErrorMessage.defaultProps = {
 ErrorMessage.propTypes = {
   title: PT.string.isRequired,
   details: PT.string,
+  support: PT.string,
   onOk: PT.func.isRequired,
 };
 
